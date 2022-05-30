591fddc7611c10dee358ebe08da078c385721ada
Xavier Mignot <code@xmi.fr>
2022-05-30 21:29:38 +0200

    chore: Upgrade to azurerm 3

--- az-functionapp.tf
+++ az-functionapp.tf
@@ -1,29 +1,24 @@
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
 
-  name = "plan-afa-${var.project}"
-  kind = "functionapp"
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  name     = "plan-afa-${var.project}"
+  os_type  = "Linux"
+  sku_name = "Y1"
 }
 
-resource "azurerm_function_app" "afa" {
+resource "azurerm_linux_function_app" "afa" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
 
   name                       = "afa-${var.project}"
-  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  service_plan_id        = azurerm_service_plan.plan.id
   storage_account_name       = azurerm_storage_account.storage_afa.name
   storage_account_access_key = azurerm_storage_account.storage_afa.primary_access_key
 
-  version    = "~3"
   https_only = true
 
-  app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.ai.instrumentation_key
+  site_config {
+    application_insights_key = azurerm_application_insights.ai.instrumentation_key
   }
 }
